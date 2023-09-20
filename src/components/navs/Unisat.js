bdb8e88ce837e4660f0e3cb13679d6403d5a5249
sededege <rankzjehz@MacBook-Pro-de-Sebastian.local>
2023-09-20 20:15:50 -0300

    asd

--- src/components/navs/Unisat.js
+++ src/components/navs/Unisat.js
@@ -2,11 +2,11 @@ import React, { useEffect, useRef, useState } from "react";
 import { Button, Card, Input, Radio } from "antd";
 import { actionType } from "../context/reducer";
 import { useStateValue } from "../context/StateProvider";
-import { getAddress, signTransaction, signMessage  } from "sats-connect";
+import { getAddress, signTransaction, signMessage } from "sats-connect";
 
 function App() {
-  const [ {ordinal} , dispatch] = useStateValue()
-  const [state, setState] = useState('')
+  const [{ ordinal }, dispatch] = useStateValue();
+  const [state, setState] = useState("");
 
   const [unisatInstalled, setUnisatInstalled] = useState(false);
   const [connected, setConnected] = useState(false);
@@ -22,26 +22,32 @@ function App() {
   const [network, setNetwork] = useState("livenet");
 
   let data = {
-    "total":10,
-    "list":[
+    total: 10,
+    list: [
       {
-        inscriptionId: '6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531i0',
+        inscriptionId:
+          "6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531i0",
         inscriptionNumber: 959941,
-        address: 'bc1q8h8s4zd9y0lkrx334aqnj4ykqs220ss735a3gh',
+        address: "bc1q8h8s4zd9y0lkrx334aqnj4ykqs220ss735a3gh",
         outputValue: 546,
-        preview: 'https://ordinals.com/preview/6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531i0',
-        content: 'https://ordinals.com/content/6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531i0',
+        preview:
+          "https://ordinals.com/preview/6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531i0",
+        content:
+          "https://ordinals.com/content/6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531i0",
         contentLength: 53,
-        contentType: 'text/plain;charset=utf-8',
+        contentType: "text/plain;charset=utf-8",
         timestamp: 1680865285,
-        genesisTransaction: '6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531',
-        location: '6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531:0:0',
-        output: '6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531:0',
-        offset: 0
-      }
-    ]
-  }
-  
+        genesisTransaction:
+          "6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531",
+        location:
+          "6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531:0:0",
+        output:
+          "6037b17df2f48cf87f6b6e6ff89af416f6f21dd3d3bc9f1832fb1ff560037531:0",
+        offset: 0,
+      },
+    ],
+  };
+
   const getBasicInfo = async () => {
     const unisat = window.unisat;
     const [address] = await unisat.getAccounts();
@@ -51,25 +57,35 @@ function App() {
     setPublicKey(publicKey);
     dispatch({
       type: actionType.SET_ORDINAL,
-    ordinal: address
-    })
+      ordinal: address,
+    });
     const balance = await unisat.getBalance();
     setBalance(balance);
 
-    const res = await unisat.getInscriptions(0,10);
-    
+    const res = await unisat.getInscriptions(0, 10);
 
     const network = await unisat.getNetwork();
     setNetwork(network);
   };
 
-  
+  const unisatwallet = (id) => {
+    console.log(id[0]);
+    setState({
+      ordinalsAddress: id[0],
+      paymentAddress: id[0],
+    });
+
+    dispatch({
+      type: actionType.SET_ORDINAL,
+      ordinal: id[0],
+    });
+  };
 
   useEffect(() => {
-  
-  }, []);
+    console.log("ordinal", ordinal);
+  }, [ordinal]);
 
- /*  if (!unisatInstalled) {
+  /*  if (!unisatInstalled) {
     return (
       <div >
           <div>
@@ -98,17 +114,17 @@ function App() {
         },
       },
       onFinish: (response) => {
-        console.log(response)
-        setConnected(true)
-     setState({
+        console.log(response);
+        setConnected(true);
+        setState({
           ordinalsAddress: response.addresses[0].address,
           paymentAddress: response.addresses[1].address,
         });
 
         dispatch({
           type: actionType.SET_ORDINAL,
-        ordinal: response.addresses[0].address,
-        })
+          ordinal: response.addresses[0].address,
+        });
       },
       onCancel: () => alert("Request canceled"),
     };
@@ -116,31 +132,50 @@ function App() {
   };
 
   return (
-    <div className="bg-black bg-opacity-80 border-2 border-orange-500 px-6  py-2 text-white rounded-lg text-[0.8rem] md:text-[1rem]">
-
-     {/*    <p>Unisat Wallet</p> */}
-
-        {connected ? (
-        <p> { state && state.ordinalsAddress.substr(0,14)}... </p> 
-        ) : (
-          <div>
-            <Button
-            className="text-white border-none items-center font-bold "
-            /*   onClick={async () => {
-                const result = await unisat.requestAccounts();
-                handleAccountsChanged(result);
-              }} */
-            onClick={() => test()}
-            >
-              Connect xVerse Wallet
-            </Button>
+    <>
+      {ordinal ? (
+        <div className="bg-black bg-opacity-80 border-2 border-orange-500 px-6  py-2 text-white rounded-lg text-[0.8rem] md:text-[1rem]">
+         
+          <p> {ordinal.substr(0, 14)}... </p>
+        </div>
+      ) : (
+        <div className="flex gap-2">
+          <div className="bg-black bg-opacity-80 border-2 border-orange-500 px-6  py-2 text-white rounded-lg text-[0.8rem] md:text-[1rem]">
+            <div className="flex gap-2">
+              <Button
+                className="text-white border-none items-center font-bold "
+                /*   onClick={async () => {
+               const result = await unisat.requestAccounts();
+               handleAccountsChanged(result);
+             }} */
+                onClick={() => test()}
+              >
+                Connect xVerse Wallet
+              </Button>
+            </div>
           </div>
-        )}
-     
-    </div>
+          <div className="bg-black bg-opacity-80 border-2 border-orange-500 px-6  py-2 text-white rounded-lg text-[0.8rem] md:text-[1rem]">
+            {/*    <p>Unisat Wallet</p> */}
+
+            <div className="flex gap-2">
+              <Button
+                className="text-white border-none items-center font-bold "
+                onClick={async () => {
+                  const result = await unisat.requestAccounts();
+                  unisatwallet(result);
+                }}
+                /*             onClick={() => test()}
+                 */
+              >
+                Connect Unisat Wallet
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
 
 /* 
 function SignMessageCard() {
@@ -173,7 +208,6 @@ function SignMessageCard() {
     </Card>
   );
 } */
-
 
 /* function SendBitcoin() {
   const [toAddress, setToAddress] = useState(
